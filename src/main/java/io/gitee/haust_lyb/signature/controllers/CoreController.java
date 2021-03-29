7fbbdd493d5074f5955eece6637ff078734d9513
liyibo <1570194845@qq.com>
2021-03-29 18:23:15 +0800

    代码结构更新

--- src/main/java/io/gitee/haust_lyb/signature/controllers/CoreController.java
+++ src/main/java/io/gitee/haust_lyb/signature/controllers/CoreController.java
@@ -1,16 +1,13 @@
 package io.gitee.haust_lyb.signature.controllers;
 
-import cn.hutool.cache.impl.TimedCache;
-import cn.hutool.core.util.StrUtil;
-import java.util.HashMap;
-
 import io.gitee.haust_lyb.signature.services.CoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
 
 @Controller
 public class CoreController {
