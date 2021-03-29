905232dd98fb6bf6b9b2e2819d1769b3ebd502e6
liyibo <1570194845@qq.com>
2021-03-29 19:29:30 +0800

    服务加密

--- src/test/java/io/gitee/haust_lyb/signature/SignatureApplicationTests.java
+++ src/test/java/io/gitee/haust_lyb/signature/SignatureApplicationTests.java
@@ -1,5 +1,6 @@
 package io.gitee.haust_lyb.signature;
 
+import io.xjar.XCryptos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SignatureApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        //
+        XCryptos.encryption()
+                .from("/Users/liyibo/IdeaProjects/signature/target/signature-0.0.1-SNAPSHOT.jar")
+                .use("haust_lyb11218888")
+                .include("/io/gitee/**/*.class")
+                .exclude("/static/**/*")
+                .exclude("/templates/**/*")
+                .to("/Users/liyibo/IdeaProjects/signature/target/encrypted/signature-0.0.1-SNAPSHOT.jar");
     }
 
 }
