4a269a686565dc0df31063f9e38a3020048f5745
liyibo <1570194845@qq.com>
2021-04-13 15:34:52 +0800

    维护

--- src/test/java/io/gitee/haust_lyb/signature/SignatureApplicationTests.java
+++ src/test/java/io/gitee/haust_lyb/signature/SignatureApplicationTests.java
@@ -10,13 +10,13 @@ class SignatureApplicationTests {
     @Test
     void contextLoads() throws Exception {
         //
-        XCryptos.encryption()
-                .from("/Users/liyibo/IdeaProjects/signature/target/signature-0.0.1-SNAPSHOT.jar")
-                .use("haust_lyb11218888")
-                .include("/io/gitee/**/*.class")
-                .exclude("/static/**/*")
-                .exclude("/templates/**/*")
-                .to("/Users/liyibo/IdeaProjects/signature/target/encrypted/signature-0.0.1-SNAPSHOT.jar");
+//        XCryptos.encryption()
+//                .from("/Users/liyibo/IdeaProjects/signature/target/signature-0.0.1-SNAPSHOT.jar")
+//                .use("haust_lyb11218888")
+//                .include("/io/gitee/**/*.class")
+//                .exclude("/static/**/*")
+//                .exclude("/templates/**/*")
+//                .to("/Users/liyibo/IdeaProjects/signature/target/encrypted/signature-0.0.1-SNAPSHOT.jar");
     }
 
 }
